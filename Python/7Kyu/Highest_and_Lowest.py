d243bfe760203e92862b0481435e25c41ba3844b
Josh's Laptop <wils1338@msu.edu>
2020-05-05 15:38:58 -0400

    Highest and Lowest

--- Python/7Kyu/Highest_and_Lowest.py
+++ Python/7Kyu/Highest_and_Lowest.py
@@ -1,6 +1,3 @@
 def high_and_low(numbers):
     num_list = [int(x) for x in numbers.split(' ')]
-    max_n = str(max(num_list))
-    min_n = str(min(num_list))
-
-    return max_n + ' ' + min_n
+    return str(max(num_list)) + ' ' + str(min(num_list))
